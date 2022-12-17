3075fbdd768bd41b31adf464c110d09747aa89cb
Robin Kautz <git@bob3000.net>
2022-12-17 14:03:16 +0100

    fix clippy errors

--- src/log_level.rs
+++ src/log_level.rs
@@ -13,7 +13,7 @@ pub enum LogLevel {
 
 impl Display for LogLevel {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "{self:?}")
     }
 }
 
